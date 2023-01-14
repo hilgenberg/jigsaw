2ebcf66d98f4b2f95182950a11ca124e12b41e2f
Torsten <th@zoon.cc>
2023-01-14 11:18:46 +0100

    Improve Hit Test

--- Puzzle_HitTest.cc
+++ Puzzle_HitTest.cc
@@ -3,22 +3,43 @@
 static inline double sqr(double x) { return x*x; }
 
 // helper function for circle-rectangle intersection
-// box of width 2rx, height 2ry, centered at origin vs circle at c with radius r
+// box of width 2w, height 2h, centered at origin vs circle at c with radius r
 #if 0 // unused
-static bool circle_vs_rect(double r, const PuzzleCoords &c, double rx, double ry)
+static bool circle_vs_rect(double r, const PuzzleCoords &c, double w, double h)
 {
-	assert(rx >= 0.0 && ry >= 0.0 && r >= 0.0);
-	if (fabs(c.y) < ry) return fabs(c.x) < rx+r;
-	if (fabs(c.x) < rx) return fabs(c.y) < ry+r;
-	return sqr(fabs(c.x)-rx)+sqr(fabs(c.y)-ry) < r*r;
+	assert(w >= 0.0 && h >= 0.0 && r >= 0.0);
+	if (fabs(c.y) < h) return fabs(c.x) < w+r;
+	if (fabs(c.x) < w) return fabs(c.y) < h+r;
+	return sqr(fabs(c.x)-w)+sqr(fabs(c.y)-h) < r*r;
 }
 #endif
-static bool ellipse_vs_rect(const PuzzleCoords &r, const PuzzleCoords &c, double rx, double ry)
+// box of width 2w, height 2h, centered at origin vs ellipse at c with radii r
+static bool ellipse_vs_rect(const PuzzleCoords &r, const PuzzleCoords &c, double w, double h)
 {
-	//same as circle_vs_rect(1.0, P2d(c.x/r.x, c.y/r.y), rx/r.x, ry/r.y);
-	if (fabs(c.y) < ry) return fabs(c.x) < rx + r.x;
-	if (fabs(c.x) < rx) return fabs(c.y) < ry + r.y;
-	return sqr((fabs(c.x)-rx)/r.x)+sqr((fabs(c.y)-ry)/r.y) < 1.0;
+	//same as circle_vs_rect(1.0, P2d(c.x/r.x, c.y/r.y), w/r.x, h/r.y);
+	if (fabs(c.y) < h) return fabs(c.x) < w + r.x;
+	if (fabs(c.x) < w) return fabs(c.y) < h + r.y;
+	return sqr((fabs(c.x)-w)/r.x)+sqr((fabs(c.y)-h)/r.y) < 1.0;
+}
+static double ellipse_vs_rect_overlap(const PuzzleCoords &r, const PuzzleCoords &c, double w, double h)
+{
+	// returns a area of their overlap, less than 0 for no intersection
+	// use rectangle instead of ellipse for now...
+	double x0 = std::max(-w, c.x-r.x), x1 = std::min(w, c.x+r.x); if (x0 >= x1) return -1.0;
+	double y0 = std::max(-h, c.y-r.y), y1 = std::min(h, c.y+r.y); if (y0 >= y1) return -1.0;
+	return (x1-x0)*(y1-y0);
+}
+// box of width 2w, height 2h, centered at origin vs ellipse at c with radii r, discounting box
+// centered at d, also width 2h and height 2h
+static double ellipse_vs_rect_rect_overlap(const PuzzleCoords &r, const PuzzleCoords &c, double w, double h, const PuzzleCoords &d)
+{
+	// first subtract the second box from our main box, leaving behind at most two pieces
+	// (because they have the same size), then sum the overlapping areas for those two pieces
+	if (fabs(d.x) >= 2.0*w || fabs(d.y) >= 2.0*h) return ellipse_vs_rect_overlap(r, c, w, h);
+
+	double dx = 0.5*d.x, dy = 0.5*d.y;
+	return ellipse_vs_rect_overlap(r, P2d(c.x-dx + (d.x >= 0.0 ? w : -w), c.y), fabs(dx), h) +
+	       ellipse_vs_rect_overlap(r, P2d(c.x-dx, c.y-dy + (d.y >= 0.0 ? h : -h)), w-dx, d.y*0.5);
 }
 
 // helper functions from the shader code (so they stay as similar as possible):
@@ -40,9 +61,15 @@ Puzzle::Piece Puzzle::hit_test(const PuzzleCoords &p, const PuzzleCoords &radius
 	// pieces, but we want to prevent that by capping the zoom at sane levels)
 	//----------------------------------------------------------------------------------
 
+	#ifdef ANDROID
+	#define FIXED is_snapped
+	#else
+	#define FIXED is_fixed
+	#endif
+
 	for (int j = N-1; j >= 0; --j)
 	{
-		const int i = z[j]; if (is_fixed(i)) continue;
+		const int i = z[j]; if (FIXED(i)) continue;
 		const double px = pos[i].x, py = pos[i].y;
 		if (x < px-d || x > px+1.0+d || y < py-d || y > py+1.0+d) continue;
 		
@@ -127,32 +154,38 @@ Puzzle::Piece Puzzle::hit_test(const PuzzleCoords &p, const PuzzleCoords &radius
 	if (radius.x < 0.01 && radius.y < 0.01) return -1; // don't bother
 	if (radius.x < 1e-6 || radius.y < 1e-6) return -1; // sanity check
 
+	int best_i = -1; double best_overlap = 0.0;
 	for (int j = N-1; j >= 0; --j)
 	{
-		const int i = z[j]; if (is_fixed(i)) continue;
+		// Pick the piece with the biggest free overlap with the finger area.
+		// Free overlap means we discount any area covered by pieces higher in
+		// the z-order. However, to keep it O(n) we only check against the 
+		// current best match, not against all other pieces.
+		
+		const int i = z[j]; if (FIXED(i)) continue;
 		const double px = pos[i].x, py = pos[i].y;
 		
-		//if (!ellipse_vs_rect(radius, P2d(x-px-0.5, y-py-0.5), 0.5+d, 0.5+d)) continue;
-		if (!ellipse_vs_rect(radius, P2d(x-px-0.5, y-py-0.5), 0.5, 0.5)) continue;
+		P2d c(x-px-0.5, y-py-0.5);
+		if (!ellipse_vs_rect(radius, c, 0.5+d, 0.5+d)) continue;
 
-		#if 0
-		const int b = (int)borders[i];
-		const int bl = tx(b);
-		const int br = tx(b>>2);
-		const int bt = tx(b>>4);
-		const int bb = tx(b>>6);
+		double a = ellipse_vs_rect_overlap(radius, c, 0.5, 0.5);
+		if (a <= best_overlap) continue;
+		assert(a > 0.0);
 
-		double x0 = 0.0, x1 = 1.0, y0 = 0.0, y1 = 1.0;
-		if (bl == 1) x0 -= d1; else if (bl == -1) x0 -= d0;
-		if (br == 1) x1 += d1; else if (br == -1) x1 += d0;
-		if (bt == 1) y0 -= d1; else if (bt == -1) y0 -= d0;
-		if (bb == 1) y1 += d1; else if (bb == -1) y1 += d0;
-		if (x-px < x0 || x-px > x1 || y-py < y0 || y-py > y1) continue;
-		#endif
+		if (best_i >= 0)
+		{
+			P2d m(pos[best_i].x-px, pos[best_i].y-py);
+			a = ellipse_vs_rect_rect_overlap(radius, c, 0.5, 0.5, m);
+			if (a <= best_overlap) continue;
+		}
 
 		rel.set(x-px, y-py);
-		return i;
-	}
+		best_i = i;
+		best_overlap = a;
 
-	return -1;
+		//if (!ellipse_vs_rect(radius, c, 0.5, 0.5)) continue;
+	}
+	#undef FIXED
+
+	return best_i;
 }
